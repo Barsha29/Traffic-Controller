4ce29714f6cfcc1617a665cdd614d89d277d453e
Barsha Parida <97872962+Barsha29@users.noreply.github.com>
2022-09-22 15:53:18 +0530

    Add files via upload

---
+++ TestBench.v
@@ -0,0 +1,26 @@
+// Code your testbench here
+// or browse Examples
+module traffic_control_tb;
+
+wire [2:0] n_lights,s_lights,e_lights,w_lights;
+reg clk,rst_a;
+
+traffic_control DUT (n_lights,s_lights,e_lights,w_lights,clk,rst_a);
+
+initial begin
+  $dumpfile( "dump.vcd" );
+  $dumpvars;
+  
+  clk=1'b1;
+  forever #5 clk=~clk;
+ end
+ 
+initial
+ begin
+  rst_a=1'b1;
+  #15;
+  rst_a=1'b0;
+  #1000;
+  $stop;
+ end
+endmodule
